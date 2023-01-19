9505e18d96e9491322a0bba5277993a2623e99bc
mitin_oleg <mitin_oa@mail.ru>
2023-01-19 08:52:00 +0300

    First commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function toReadable (number) {
-  
+    var converter = require('number-to-words');
+    word = converter.toWords(number);
+    word = word.replace(/[-]/g, ' ');
+    return word;
 }
